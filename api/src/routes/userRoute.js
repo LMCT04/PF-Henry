2c776f88dc937f76e709afdebe6277549f4de369
DanielMariscal <mariscaldaniel@gmail.com>
2023-06-14 11:09:10 -0400

    rutaGetUser

--- api/src/routes/userRoute.js
+++ api/src/routes/userRoute.js
@@ -1,11 +1,34 @@
 const { Router } = require("express");
 const userRoute = Router();
+const { User } = require("../database");
+userRoute.get("/",async(req,res)=>{
+  try {
+    let users = await User.findAll();
+    res.status(200).send(users);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 userRoute.post("/", async (req, res) => {
-  try {
-    //aca redireccionar al middleware que trabajara con esta ruta, por ejemplo:
-    //const product = await postCreateUser()
-  } catch (error) {}
+    try {  
+      // Insertar un nuevo registro
+      const newUser = await User.create({
+        image: "https://example.com/imagen.jpg",
+        name: "John",
+        lastName: "Doe",
+        mail: "johndoe@example.com",
+        password: "secretpassword",
+        age: 25,
+        address: "123 Main St",
+        favorite: [1, 2, 3],
+        shoppingHistory: [4, 5, 6],
+      });
+      res.status(200).send("Nuevo usuario insertado:", newUser);
+    } catch (error) {
+      console.error("Error al insertar usuario:", error);
+    }
+  
 });
 
 module.exports = userRoute;
