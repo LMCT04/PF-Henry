0cd57b7b364451e0d19bfb58fc1964220fd33292
Juan9999K <juandavidquimbayo99@gmail.com>
2023-06-14 17:32:09 -0500

    "funcionalidades del menu"

--- client/src/Views/Menu/menu.jsx
+++ client/src/Views/Menu/menu.jsx
@@ -1,7 +1,44 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { orderAlphabetic, orderPrice } from "../../redux/actions/actionsProducts";
+import Loading from "../Loading/Loading";
+import Card from "../../Components/cards/Card";
+import style from "./menu.module.css";
+
 const Menu = () => {
-    return (
-        <h1>Estoy en Menu</h1>
-    )
-}
+  const dispatch = useDispatch();
+  const allProducts = useSelector((state) => state.products || []);
+
+  const handleAlphabeticOrder = (e) => {
+    const value = e.target.value;
+    dispatch(orderAlphabetic(value));
+  };
+
+  const handlePriceOrder = (e) => {
+    const value = e.target.value;
+    dispatch(orderPrice(value));
+  };
+
+  return (
+    <div className={style.cardContainer}>
+      <label>Alphabetic order</label>
+      <select onChange={handleAlphabeticOrder}>
+        <option value="asc">ASC</option>
+        <option value="desc">DESC</option>
+      </select>
+      <label>PRICE ORDER</label>
+      <select onChange={handlePriceOrder}>
+        <option value="asc">ASC</option>
+        <option value="desc">DESC</option>
+      </select>
+
+      {allProducts.length > 0 ? (
+        allProducts.map((e) => <Card key={e.id} element={e} />)
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
+};
 
 export default Menu;
