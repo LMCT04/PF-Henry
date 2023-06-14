aa7ecf486188b0c865bef579606fafc95886216a
Julian Llop <juliianllop@gmail.com>
2023-06-14 11:50:31 -0300

    Home View

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import NavBar from '../../Components/NavBar/navBar';
-import Sliders from '../../Components/Sliders/Sliders';
-import Footer from '../../Components/Footer/Footer';
+import React from "react";
+import NavBar from "../../Components/NavBar/navBar";
+import Sliders from "../../Components/Sliders/Sliders";
+import Footer from "../../Components/Footer/Footer";
+import style from "./Home.module.css";
 
 const Home = () => {
     return (
-        <div>
-            <NavBar/>
-            <Sliders/>
-            <Footer/>
+        <div className={style.container}>
+            <div className={style.home}>
+                <Sliders />
+            </div>
+            <Footer />
         </div>
     );
-}
+};
 
 export default Home;
