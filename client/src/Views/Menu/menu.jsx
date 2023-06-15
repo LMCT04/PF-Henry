6339683d2abdffb3ca423bb001d15445610990c0
DanielMariscal <mariscaldaniel@gmail.com>
2023-06-15 12:27:37 -0400

    Paganiation v1

--- client/src/Views/Menu/menu.jsx
+++ client/src/Views/Menu/menu.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { orderAlphabetic, orderPrice } from "../../redux/actions/actionsProducts";
+import {
+  orderAlphabetic,
+  orderPrice,
+} from "../../redux/actions/actionsProducts";
 import Loading from "../Loading/Loading";
 import Card from "../../Components/cards/Card";
 import style from "./menu.module.css";
-
+import Pagination from "@mui/material/Pagination";
 const Menu = () => {
   const dispatch = useDispatch();
   const allProducts = useSelector((state) => state.products || []);
+
+  const [pageProducts, setPageProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const handleChange = (event, value) => {
+    let productsPag=[...allProducts];
+    setPage(value);
+    const startIndex = (value - 1) * 10;
+    const endIndex = startIndex + 10;
+    setPageProducts(productsPag.slice(startIndex, endIndex));
+  };
+
+  React.useEffect(() => {
+    let productsPag = [...allProducts];
+    const startIndex = (page - 1) * 10;
+    const endIndex = startIndex + 10;
+    setPageProducts(productsPag.slice(startIndex, endIndex));
+  }, [allProducts, page]);
 
   const handleAlphabeticOrder = (e) => {
     const value = e.target.value;
@@ -32,13 +52,21 @@ const Menu = () => {
         <option value="desc">DESC</option>
       </select>
 
-      {allProducts.length > 0 ? (
-        allProducts.map((e) => <Card key={e.id} element={e} />)
+      {pageProducts.length > 0 ? (
+        pageProducts.map((e) => <Card key={e.id} element={e} />)
       ) : (
         <Loading />
       )}
+      <Pagination
+        count={Math.ceil(allProducts.length / 10)}
+        variant="outlined"
+        color="primary"
+        onChange={handleChange}
+      />
     </div>
   );
 };
 
 export default Menu;
+
+
