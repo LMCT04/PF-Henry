659cdc81ca21d43f4613d9706b84e520d198b1bf
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2023-06-16 01:50:03 -0300

    Fix-Feature(cleint): bugs-clean-fix

--- client/src/Views/Menu/menu.jsx
+++ client/src/Views/Menu/menu.jsx
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  orderAlphabetic,
-  orderPrice,
-} from "../../redux/actions/actionsProducts";
-import Loading from "../Loading/Loading";
-import Card from "../../Components/cards/Card";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import style from "./menu.module.css";
-import Pagination from "@mui/material/Pagination";
-//import Footer from "../../Components/Footer/Footer";
 import { getAllProducts } from '../../redux/actions/actionsProducts'
+import CardsContainer from "../../Components/cardContainer/cardContainer";
+import Footer from '../../Components/Footer/Footer'
 
 
 const Menu = () => {
@@ -19,61 +13,10 @@ const Menu = () => {
     dispatch(getAllProducts())
   }, [dispatch])
 
-  const allP = useSelector((state ) => state.product)
-
-  //const allProducts = useSelector((state) => state.products || []);
-
-  const [pageProducts, setPageProducts] = useState([]);
-  const [page, setPage] = useState(1);
-  const handleChange = (event, value) => {
-    let productsPag=[...allP];
-    setPage(value);
-    const startIndex = (value - 1) * 10;
-    const endIndex = startIndex + 10;
-    setPageProducts(productsPag.slice(startIndex, endIndex));
-  };
-
-  React.useEffect(() => {
-    let productsPag = [...allP];
-    const startIndex = (page - 1) * 10;
-    const endIndex = startIndex + 10;
-    setPageProducts(productsPag.slice(startIndex, endIndex));
-  }, [allP, page]);
-
-  const handleAlphabeticOrder = (e) => {
-    const value = e.target.value;
-    dispatch(orderAlphabetic(value));
-  };
-
-  const handlePriceOrder = (e) => {
-    const value = e.target.value;
-    dispatch(orderPrice(value));
-  };
-
   return (
-    <div className={style.cardContainer}>
-      <label>Alphabetic order</label>
-      <select onChange={handleAlphabeticOrder}>
-        <option value="asc">ASC</option>
-        <option value="desc">DESC</option>
-      </select>
-      <label>PRICE ORDER</label>
-      <select onChange={handlePriceOrder}>
-        <option value="asc">ASC</option>
-        <option value="desc">DESC</option>
-      </select>
-
-      {pageProducts.length > 0 ? (
-        pageProducts.map((e) => <Card key={e.id} element={e} />)
-      ) : (
-        <Loading />
-      )}
-      <Pagination
-        count={Math.ceil(allP.length / 10)}
-        variant="outlined"
-        color="primary"
-        onChange={handleChange}
-      />
+    <div className={style.container} >
+      <CardsContainer/>
+      <Footer/>
     </div>
   );
 };
