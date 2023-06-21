1c50032b3b9eebe1d9c5e6cba3965e7881b072d2
Fernando Lorenzo <feer.lorenzoo@hotmail.com>
2023-06-21 16:14:59 -0300

    fix (db): add relation

--- api/src/database.js
+++ api/src/database.js
@@ -12,8 +12,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
   );*/
 
 const sequelize = new Sequelize(DB_DEPLOY, {
-    logging: false,
-    native: false,
+  logging: false,
+  native: false,
 });
 
 const basename = path.basename(__filename);
@@ -21,31 +21,32 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 fs.readdirSync(path.join(__dirname, "/models"))
-    .filter(
-        (file) =>
-            file.indexOf(".") !== 0 &&
-            file !== basename &&
-            file.slice(-3) === ".js"
-    )
-    .forEach((file) => {
-        modelDefiners.push(require(path.join(__dirname, "/models", file)));
-    });
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
 
 modelDefiners.forEach((model) => model(sequelize));
 
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
-    entry[0][0].toUpperCase() + entry[0].slice(1),
-    entry[1],
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
 ]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 //Relacionar modelos
-const { User, Product } = sequelize.models;
+const { User, Product, Category } = sequelize.models;
 
 //Relaciones
 User.hasMany(Product);
+Product.belongsToMany(Category, { through: "categoryProducts" });
+Category.belongsToMany(Product, { through: "categoryProducts" });
+
 module.exports = {
-    ...sequelize.models,
-    conn: sequelize,
+  ...sequelize.models,
+  conn: sequelize,
 };
