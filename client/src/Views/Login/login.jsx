4003f5e7b29adae8275a175d1e162e76cfcbf423
Duvan Cruz <duvandres9820@gmail.com>
2023-06-22 15:47:33 -0500

    autentificacion con google

--- client/src/Views/Login/login.jsx
+++ client/src/Views/Login/login.jsx
@@ -1,161 +1,152 @@
-import style from "./login.module.css";
-//import { Link } from 'react-router-dom'
-import { Formik } from "formik";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { useState } from "react";
+import { Formik } from "formik";
 import { TextField, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import style from "./login.module.css";
 
 const Login = () => {
-    const history = useHistory();
-    const [error, setError] = useState("");
+  const history = useHistory();
+  const [error, setError] = useState("");
 
-    const handleRegister = () => {
-        history.push("/register");
-    };
+  const handleRegister = () => {
+    history.push("/register");
+  };
 
-    const users = [
-        {
-            username: "pepito",
-            password: "pepito1234",
-        },
-        {
-            username: "pablito",
-            password: "pablito1234",
-        },
-        {
-            username: "carmen",
-            password: "carmen1234",
-        },
-        {
-            username: "juana",
-            password: "juana1234",
-        },
-        {
-            username: "miguel",
-            password: "miguel1234",
-        },
-        {
-            username: "adminJulian",
-            password: "password98",
-        },
-    ];
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithProvider(provider);
+  };
 
-    return (
-        <div className={style.loginContainer}>
-            <Formik
-                initialValues={{
-                    username: "",
-                    password: "",
-                }}
-                validate={(valores) => {
-                    let errores = {};
+  const signInWithProvider = async (provider) => {
+    try {
+      const authInstance = getAuth();
+      await signInWithPopup(authInstance, provider);
+      history.push("/menu");
+    } catch (error) {
+      setError("An error occurred during login");
+    }
+  };
 
-                    //Validacion Username
-                    if (!valores.username) {
-                        errores.username = "Please enter your username";
-                    } else if (
-                        !/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.username)
-                    ) {
-                        errores.username =
-                            "You only need to enter letters and spaces";
-                    }
+  const users = [
+    {
+      username: "pepito",
+      password: "pepito1234",
+    },
+    {
+      username: "pablito",
+      password: "pablito1234",
+    },
+    // ... otros usuarios
+  ];
 
-                    //Validacion Password
-                    if (!valores.password) {
-                        errores.password = "Please enter your password";
-                    }
+  return (
+    <div className={style.loginContainer}>
+      <Formik
+        initialValues={{
+          username: "",
+          password: "",
+        }}
+        validate={(values) => {
+          let errors = {};
 
-                    return errores;
-                }}
-                onSubmit={(valores, { resetForm }) => {
-                    resetForm();
-                    console.log("comprobar usuario");
-                    console.log(valores);
+          // Validación Username
+          if (!values.username) {
+            errors.username = "Please enter your username";
+          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.username)) {
+            errors.username = "You only need to enter letters and spaces";
+          }
 
-                    const foundUser = users.find(
-                        (user) =>
-                            user.username === valores.username &&
-                            user.password === valores.password
-                    );
-                    if (foundUser) {
-                        history.push("/menu");
-                    } else {
-                        setError("The user entered does not exist");
-                    }
-                }}
-            >
-                {({
-                    handleSubmit,
-                    values,
-                    handleChange,
-                    handleBlur,
-                    errors,
-                    touched,
-                }) => (
-                    <form onSubmit={handleSubmit} className={style.form}>
-                        <section className={style.content}>
-                            <h1>Sign in:</h1>
-                            <div>
-                                <TextField
-                                    InputLabelProps={{ shrink: true }}
-                                    fullWidth
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    label="Username"
-                                    value={values.username}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    error={
-                                        touched.username && !!errors.username
-                                    }
-                                    helperText={
-                                        touched.username && errors.username
-                                    }
-                                    color="success"
-                                />
-                            </div>
-                            <div>
-                                <TextField
-                                    InputLabelProps={{ shrink: true }}
-                                    fullWidth
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    label="Password"
-                                    value={values.password}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    error={
-                                        touched.password && !!errors.password
-                                    }
-                                    helperText={
-                                        touched.password && errors.password
-                                    }
-                                    color="success"
-                                />
-                            </div>
-                            <div>
-                                <Button
-                                    fullWidth
-                                    variant="contained"
-                                    type="submit"
-                                    color="success"
-                                >
-                                    Login
-                                </Button>
-                            </div>
-                            <Link to="/register" className={style.link}>
-                                Don't have an account?
-                            </Link>
-                        </section>
-                    </form>
-                )}
-            </Formik>
-            {error && <p>{error}</p>}
-        </div>
-    );
+          // Validación Password
+          if (!values.password) {
+            errors.password = "Please enter your password";
+          }
+
+          return errors;
+        }}
+        onSubmit={(values, { resetForm }) => {
+          resetForm();
+          console.log("comprobar usuario");
+          console.log(values);
+
+          const foundUser = users.find(
+            (user) =>
+              user.username === values.username &&
+              user.password === values.password
+          );
+          if (foundUser) {
+            history.push("/menu");
+          } else {
+            setError("The user entered does not exist");
+          }
+        }}
+      >
+        {({ handleSubmit, values, handleChange, handleBlur, errors, touched }) => (
+          <form onSubmit={handleSubmit} className={style.form}>
+            <section className={style.content}>
+              <h1>Sign in:</h1>
+              <div>
+                <TextField
+                  InputLabelProps={{ shrink: true }}
+                  fullWidth
+                  type="text"
+                  id="username"
+                  name="username"
+                  label="Username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.username && !!errors.username}
+                  helperText={touched.username && errors.username}
+                  color="success"
+                />
+              </div>
+              <div>
+                <TextField
+                  InputLabelProps={{ shrink: true }}
+                  fullWidth
+                  type="password"
+                  id="password"
+                  name="password"
+                  label="Password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                  color="success"
+                />
+              </div>
+              <div className={style.actions}>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  type="submit"
+                  className={style.button}
+                >
+                  Sign In
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="success"
+                  onClick={handleGoogleLogin}
+                  className={style.button}
+                >
+                  Sign In with Google
+                </Button>
+              </div>
+              <div className={style.registerLink}>
+                <Link to="/register">Don't have an account? Register here</Link>
+              </div>
+            </section>
+          </form>
+        )}
+      </Formik>
+      {error && <div className={style.error}>{error}</div>}
+    </div>
+  );
 };
 
 export default Login;
