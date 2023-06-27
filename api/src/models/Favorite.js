16781c70acecdbcb1b35a2be16873b69ed3b7932
Fernando Lorenzo <feer.lorenzoo@hotmail.com>
2023-06-27 16:05:31 -0300

    feat(api/models) add new models.

---
+++ api/src/models/Favorite.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "favorite",
+    {
+      productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
---
+++ api/src/models/ShoppingCart.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("shoppingCart", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    totalPrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+  });
+};
