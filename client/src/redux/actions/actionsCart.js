3a9751498915352f52207fecb40b6189ade8e8a0
JuanCarlosJ <93050072+juancjerezm@users.noreply.github.com>
2023-06-29 18:50:10 -0500

    fix(reducer)solucionado un bug en el reducer

--- client/src/redux/actions/actionsCart.js
+++ client/src/redux/actions/actionsCart.js
@@ -1,84 +1,84 @@
 import {
-    ADD_TO_CART,
-    REMOVE_FROM_CART,
-    CLEAR_CART,
-    GET_CART_BY_ID,
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CLEAR_CART,
+  GET_CART_BY_ID,
 } from "../actionsType/cartAT";
 import { axios } from "axios";
 
 export const addToCart = (payload) => {
-    return async (dispatch) => {
-        try {
-            const response = await axios.post(
-                "http://localhost:3001/cart/add",
-                payload
-            );
-            const addedProduct = response.data;
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/cart/add",
+        payload
+      );
+      const addedProduct = response.data;
 
-            dispatch({
-                type: ADD_TO_CART,
-                payload: addedProduct,
-            });
-        } catch (error) {
-            console.log("Error: addProduct", error);
-            alert("Hubo un error al agregar el producto al carrito.");
-        }
-    };
+      dispatch({
+        type: ADD_TO_CART,
+        payload: addedProduct,
+      });
+    } catch (error) {
+      console.log("Error: addProduct", error);
+      alert("Hubo un error al agregar el producto al carrito.");
+    }
+  };
 };
 
 export const removeFromCart = (payload) => {
-    return async (dispatch) => {
-        try {
-            const response = await axios.post(
-                "http://localhost:3001/cart/remove",
-                payload
-            );
-            const removedProduct = response.data;
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/cart/remove",
+        payload
+      );
+      const removedProduct = response.data;
 
-            dispatch({
-                type: REMOVE_FROM_CART,
-                payload: removedProduct,
-            });
-        } catch (error) {
-            console.log("Error: removeProduct", error);
-            alert("Hubo un error al remover el producto del carrito.");
-        }
-    };
+      dispatch({
+        type: REMOVE_FROM_CART,
+        payload: removedProduct,
+      });
+    } catch (error) {
+      console.log("Error: removeProduct", error);
+      alert("Hubo un error al remover el producto del carrito.");
+    }
+  };
 };
 
 export const getCartById = (id) => {
-    return async (dispatch) => {
-        try {
-            const apiData = await axios.get(`http://localhost:3001/cart/${id}`);
-            const cart = apiData.data;
+  return async (dispatch) => {
+    try {
+      const apiData = await axios.get(`http://localhost:3001/cart/${id}`);
+      const cart = apiData.data;
 
-            dispatch({
-                type: GET_CART_BY_ID,
-                payload: cart,
-            });
-        } catch (error) {
-            console.log("Error: getCartById", error);
-            alert("No se encontró ningún carrito con ese ID.");
-        }
-    };
+      dispatch({
+        type: GET_CART_BY_ID,
+        payload: cart,
+      });
+    } catch (error) {
+      console.log("Error: getCartById", error);
+      alert("No se encontró ningún carrito con ese ID.");
+    }
+  };
 };
 
 export const clearCart = () => {
-    return async (dispatch) => {
-        try {
-            const response = await axios.post(
-                "http://localhost:3001/cart/clear",
-                payload
-            );
-            const clearedCart = response.data;
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/cart/clear",
+        payload
+      );
+      const clearedCart = response.data;
 
-            dispatch({
-                type: REMOVE_FROM_CART,
-                payload: clearedCart,
-            });
-        } catch (error) {
-            console.log("Error: removeProduct", error);
-            alert("Hubo un error al limpiar el carrito.");
-        }
-    };
+      dispatch({
+        type: CLEAR_CART,
+        payload: clearedCart,
+      });
+    } catch (error) {
+      console.log("Error: removeProduct", error);
+      alert("Hubo un error al limpiar el carrito.");
+    }
+  };
 };
