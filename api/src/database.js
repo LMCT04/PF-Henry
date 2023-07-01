2f0bba291960f5afc550d19fff765a6909d35518
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2023-07-01 16:10:40 -0300

    deployfixdb

--- api/src/database.js
+++ api/src/database.js
@@ -3,13 +3,10 @@ const { Sequelize, HasOne } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/spacafeteria`,
-  {
+const sequelize = new Sequelize(DB_DEPLOY, {
     logging: false,
     native: false,
-  }
-);
+});
 
 // const sequelize = new Sequelize(DB_DEPLOY, {
 //   logging: false,
@@ -21,32 +18,34 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 fs.readdirSync(path.join(__dirname, "/models"))
-  .filter(
-    (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-  )
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
-  });
+    .filter(
+        (file) =>
+            file.indexOf(".") !== 0 &&
+            file !== basename &&
+            file.slice(-3) === ".js"
+    )
+    .forEach((file) => {
+        modelDefiners.push(require(path.join(__dirname, "/models", file)));
+    });
 
 modelDefiners.forEach((model) => model(sequelize));
 
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
-  entry[0][0].toUpperCase() + entry[0].slice(1),
-  entry[1],
+    entry[0][0].toUpperCase() + entry[0].slice(1),
+    entry[1],
 ]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 //Relacionar modelos
 const {
-  User,
-  Product,
-  Category,
-  ShoppingCart,
-  PuchaseOrder,
-  Favorite,
-  CartProduct,
+    User,
+    Product,
+    Category,
+    ShoppingCart,
+    PuchaseOrder,
+    Favorite,
+    CartProduct,
 } = sequelize.models;
 
 //Relaciones
@@ -64,6 +63,6 @@ ShoppingCart.belongsToMany(Product, { through: "cartProduct" }); //ShoppingCart 
 Product.belongsToMany(ShoppingCart, { through: "cartProduct" });
 
 module.exports = {
-  ...sequelize.models,
-  conn: sequelize,
+    ...sequelize.models,
+    conn: sequelize,
 };
