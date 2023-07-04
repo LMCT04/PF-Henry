f77db9a919b9e8059e88858d0c644f7e3077b319
Julian Llop <juliianllop@gmail.com>
2023-07-04 19:01:07 -0300

    (fix) cart funcionando

--- client/src/Components/NavBar/navBar.jsx
+++ client/src/Components/NavBar/navBar.jsx
@@ -8,225 +8,224 @@ import { Box, Button, Menu, MenuItem } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import Badge from "@mui/material/Badge";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getCartById } from "../../redux/actions/actionsCart";
 
 const NavBar = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const quantity = useSelector((state) => state.shoppingCart.cart.quantity);
-  const userId = useSelector((state) => state.user.id);
+    const history = useHistory();
+    const shoppingCart = useSelector((state) => state.shoppingCart);
+    const userId = useSelector((state) => state.user.id);
+    const quantity = useSelector((state) => state?.shoppingCart?.cart?.quantity);
 
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  useEffect(() => {
-    dispatch(getCartById(userId));
-  }, [dispatch]);
+    const [anchorEl, setAnchorEl] = useState(null);
 
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleLoginClick = () => {
-    history.push("/login");
-  };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-  const handleRegisterClick = () => {
-    history.push("/register");
-  };
+    const handleLoginClick = () => {
+        history.push("/login");
+    };
 
-  const handleProfileClick = () => {
-    history.push("/profile");
-  };
+    const handleRegisterClick = () => {
+        history.push("/register");
+    };
 
-  const handlePageClick = () => {
-    history.push("/");
-  };
+    const handleProfileClick = () => {
+        history.push("/profile");
+    };
 
-  const handleDashboardClick = () => {
-    history.push("/dashboard");
-  };
+    const handlePageClick = () => {
+        history.push("/");
+    };
 
-  const handleCartClick = () => {
-    history.push(`/cart/${userId}`);
-  };
+    const handleDashboardClick = () => {
+        history.push("/dashboard");
+    };
 
-  return (
-    <Box
-      sx={{
-        backgroundColor: "#2c6e49",
-        height: "8vh",
-        display: "flex",
-      }}
-    >
-      <Box
-        sx={{
-          width: "15%",
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
-      >
-        <button className={style.logoBtn}>
-          <img src={logo} alt="logos" className={style.logo} />
-        </button>
-      </Box>
+    const handleCartClick = () => {
+        history.push(`/cart/${userId}`);
+    };
 
-      <Box
-        sx={{
-          width: "25%",
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <NavLink
-          to="/"
-          exact
-          className={style.navLink}
-          style={(isActive) => ({
-            fontSize: isActive ? "20px" : "20px",
-            textDecoration: isActive ? "none" : "none",
-            color: isActive ? "#fefee3" : "#4c956c",
-          })}
-        >
-          HOME
-        </NavLink>
-
-        <NavLink
-          to="/menu"
-          className={style.navLink}
-          style={(isActive) => ({
-            fontSize: isActive ? "20px" : "20px",
-            textDecoration: isActive ? "none" : "none",
-            color: isActive ? "#fefee3" : "#4c956c",
-          })}
-        >
-          MENU
-        </NavLink>
-      </Box>
-
-      <Box
-        sx={{
-          width: "60%",
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
-        }}
-      >
-        <Button
-          sx={{
-            backgroundColor: "#2c6e49",
-            border: "2px solid #fefee3",
-            color: "#fefee3",
-            "&:hover": {
-              backgroundColor: "#fefee3",
-              color: "#2c6e49",
-              fontWeight: "bold",
-            },
-            width: "10%",
-            height: "50%",
-            marginRight: "5%",
-            fontFamily: "Roboto Mono, monospace",
-            borderRadius: "25px",
-          }}
-          onClick={handleLoginClick}
-        >
-          LOGIN IN
-        </Button>
-        <Button
-          sx={{
-            backgroundColor: "#fefee3",
-            border: "2px solid #fefee3",
-            color: "#2c6e49",
-            "&:hover": {
-              backgroundColor: "#2c6e49",
-              border: "2px solid #fefee3",
-              color: "#fefee3",
-              fontWeight: "bold",
-            },
-            width: "10%",
-            height: "50%",
-            marginRight: "5%",
-            fontFamily: "Roboto Mono, monospace",
-            borderRadius: "25px",
-          }}
-          onClick={handleRegisterClick}
-        >
-          JOIN NOW
-        </Button>
-        <Button
-          sx={{
-            color: " #fefee3",
-            marginRight: "5%",
-            borderRadius: "50px",
-            fontWeight: "bold",
-            fontFamily: "Roboto Mono, monospace",
-          }}
-          onClick={handleClick}
-        >
-          <AccountCircleOutlinedIcon
+    return (
+        <Box
             sx={{
-              fontSize: "45px",
-              margin: "0",
-              padding: "0",
-              "&:hover": {
-                color: " #fefee3af",
-              },
+                backgroundColor: "#2c6e49",
+                height: "8vh",
+                display: "flex",
             }}
-          />
-        </Button>
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
         >
-          <MenuItem onClick={handleProfileClick}>MY ACCOUNT</MenuItem>
+            <Box
+                sx={{
+                    width: "15%",
+                    display: "flex",
+                    justifyContent: "flex-end",
+                }}
+            >
+                <button className={style.logoBtn}>
+                    <img src={logo} alt="logos" className={style.logo} />
+                </button>
+            </Box>
 
-          <MenuItem onClick={handleDashboardClick}>DASHBOARD</MenuItem>
+            <Box
+                sx={{
+                    width: "25%",
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                }}
+            >
+                <NavLink
+                    to="/"
+                    exact
+                    className={style.navLink}
+                    style={(isActive) => ({
+                        fontSize: isActive ? "20px" : "20px",
+                        textDecoration: isActive ? "none" : "none",
+                        color: isActive ? "#fefee3" : "#4c956c",
+                    })}
+                >
+                    HOME
+                </NavLink>
 
-          <MenuItem onClick={handlePageClick}>WEB PAGE</MenuItem>
+                <NavLink
+                    to="/menu"
+                    className={style.navLink}
+                    style={(isActive) => ({
+                        fontSize: isActive ? "20px" : "20px",
+                        textDecoration: isActive ? "none" : "none",
+                        color: isActive ? "#fefee3" : "#4c956c",
+                    })}
+                >
+                    MENU
+                </NavLink>
+            </Box>
 
-          <MenuItem>LOG OUT</MenuItem>
-        </Menu>
+            <Box
+                sx={{
+                    width: "60%",
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                }}
+            >
+                <Button
+                    sx={{
+                        backgroundColor: "#2c6e49",
+                        border: "2px solid #fefee3",
+                        color: "#fefee3",
+                        "&:hover": {
+                            backgroundColor: "#fefee3",
+                            color: "#2c6e49",
+                            fontWeight: "bold",
+                        },
+                        width: "10%",
+                        height: "50%",
+                        marginRight: "5%",
+                        fontFamily: "Roboto Mono, monospace",
+                        borderRadius: "25px",
+                    }}
+                    onClick={handleLoginClick}
+                >
+                    LOGIN IN
+                </Button>
+                <Button
+                    sx={{
+                        backgroundColor: "#fefee3",
+                        border: "2px solid #fefee3",
+                        color: "#2c6e49",
+                        "&:hover": {
+                            backgroundColor: "#2c6e49",
+                            border: "2px solid #fefee3",
+                            color: "#fefee3",
+                            fontWeight: "bold",
+                        },
+                        width: "10%",
+                        height: "50%",
+                        marginRight: "5%",
+                        fontFamily: "Roboto Mono, monospace",
+                        borderRadius: "25px",
+                    }}
+                    onClick={handleRegisterClick}
+                >
+                    JOIN NOW
+                </Button>
+                <Button
+                    sx={{
+                        color: " #fefee3",
+                        marginRight: "5%",
+                        borderRadius: "50px",
+                        fontWeight: "bold",
+                        fontFamily: "Roboto Mono, monospace",
+                    }}
+                    onClick={handleClick}
+                >
+                    <AccountCircleOutlinedIcon
+                        sx={{
+                            fontSize: "45px",
+                            margin: "0",
+                            padding: "0",
+                            "&:hover": {
+                                color: " #fefee3af",
+                            },
+                        }}
+                    />
+                </Button>
+                <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "right",
+                    }}
+                    transformOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                    }}
+                >
+                    <MenuItem onClick={handleProfileClick}>MY ACCOUNT</MenuItem>
 
-        <Button
-          sx={{
-            color: " #fefee3af",
-            marginRight: "5%",
-            borderRadius: "50px",
-          }}
-          onClick={handleCartClick}
-        >
-          <Badge badgeContent={quantity} color="primary">
-            <ShoppingCartIcon
-              sx={{
-                color: "#fefee3",
-                fontSize: "40px",
-                "&:hover": {
-                  color: " #fefee3af",
-                },
-                marginRight: "5%",
-                fontFamily: "Roboto Mono, monospace",
-              }}
-            />
-          </Badge>
-        </Button>
-      </Box>
-    </Box>
-  );
+                    <MenuItem onClick={handleDashboardClick}>
+                        DASHBOARD
+                    </MenuItem>
+
+                    <MenuItem onClick={handlePageClick}>WEB PAGE</MenuItem>
+
+                    <MenuItem>LOG OUT</MenuItem>
+                </Menu>
+
+                <Button
+                    sx={{
+                        color: " #fefee3af",
+                        marginRight: "5%",
+                        borderRadius: "50px",
+                    }}
+                    onClick={handleCartClick}
+                >
+                    <Badge badgeContent={quantity} color="primary">
+                        <ShoppingCartIcon
+                            sx={{
+                                color: "#fefee3",
+                                fontSize: "40px",
+                                "&:hover": {
+                                    color: " #fefee3af",
+                                },
+                                marginRight: "5%",
+                                fontFamily: "Roboto Mono, monospace",
+                            }}
+                        />
+                    </Badge>
+                </Button>
+            </Box>
+        </Box>
+    );
 };
 
 export default NavBar;
