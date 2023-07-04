7ccec4d26d1bfe3769c7676308ca93461fcc9c8a
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2023-07-04 12:58:09 -0300

    7/4

--- api/src/routes/middleware/userFunct.js
+++ api/src/routes/middleware/userFunct.js
@@ -1,80 +1,88 @@
 const {
-  modelgetUserFromDatabase,
-  modelpostUserInDatabase,
-  modelgetAllUserFromDatabase,
-  modelupdatePasswordInDatabase,
-  modelupdateUsernameInDatabase,
+    modelgetUserFromDatabase,
+    modelpostUserInDatabase,
+    modelgetAllUserFromDatabase,
+    modelupdatePasswordInDatabase,
+    modelupdateUsernameInDatabase,
+    modelgetAllUserFromDatabasebyName,
 } = require("./modules/CRUDUser");
 
 const updateUserInDatabase = async (mail, password, userName) => {
-  try {
-    const user = await modelgetUserFromDatabase(mail);
+    try {
+        const user = await modelgetUserFromDatabase(mail);
 
-    if (!user) {
-      throw new Error("Usuario no encontrado");
+        if (!user) {
+            throw new Error("Usuario no encontrado");
+        }
+
+        if (password) {
+            await modelupdatePasswordInDatabase(mail, password);
+        }
+
+        if (userName) {
+            await modelupdateUsernameInDatabase(mail, userName);
+        }
+
+        return user;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error al actualizar el usuario");
     }
-
-    if (password) {
-      await modelupdatePasswordInDatabase(mail, password);
-    }
-
-    if (userName) {
-      await modelupdateUsernameInDatabase(mail, userName);
-    }
-
-    return user;
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error al actualizar el usuario");
-  }
 };
 
-const getUser = async (mail) => {
-  try {
-    const databaseUser = await (mail
-      ? modelgetUserFromDatabase(mail)
-      : modelgetAllUserFromDatabase());
-    return databaseUser;
-  } catch (error) {
-    console.error("Error al obtener el usuario:", error);
-    throw new Error("Error al obtener el usuario");
-  }
+const getUser = async (mail, fullName) => {
+    try {
+        let databaseUser;
+
+        if (mail) {
+            databaseUser = await modelgetUserFromDatabase(mail);
+        } else if (fullName) {
+            databaseUser = await modelgetAllUserFromDatabasebyName(fullName);
+        } else {
+            databaseUser = await modelgetAllUserFromDatabase();
+        }
+
+        return databaseUser;
+    } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+        throw new Error("Error al obtener el usuario");
+    }
 };
 
 const postUser = async (
-  fullName,
-  userName,
-  image,
-  mail,
-  password,
-  age,
-  address,
-  favorite,
-  shoppingHistory,
-  role
+    fullName,
+    userName,
+    image,
+    mail,
+    password,
+    age,
+    address,
+    favorite,
+    shoppingHistory,
+    role
 ) => {
-  try {
-    const createUsuario = await modelpostUserInDatabase(
-      fullName,
-      userName,
-      image,
-      mail,
-      password,
-      age,
-      address,
-      favorite,
-      shoppingHistory,
-      role
-    );
-    return createUsuario;
-  } catch (error) {
-    console.error("Error al crear al usuario:", error);
-    throw new Error("Error al crear el usuario");
-  }
+    try {
+        const createUsuario = await modelpostUserInDatabase(
+            fullName,
+            userName,
+            image,
+            mail,
+            password,
+            age,
+            address,
+            favorite,
+            shoppingHistory,
+            role
+        );
+        return createUsuario;
+    } catch (error) {
+        console.error("Error al crear al usuario:", error);
+        throw new Error("Error al crear el usuario");
+    }
 };
 
 module.exports = {
-  getUser,
-  postUser,
-  updateUserInDatabase,
+    getUser,
+    postUser,
+    updateUserInDatabase,
 };
