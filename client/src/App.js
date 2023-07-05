9833ce7e3d9f6a0ecd49a0644adfa382d5458012
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2023-07-05 13:57:10 -0300

    CSS-LOGIN-reg

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import { Route, Switch, useLocation } from "react-router-dom";
 import NavBar from "./Components/NavBar/navBar";
 import {
-  Landing,
-  Register,
-  Menu,
-  Login,
-  Form,
-  About,
-  Detail,
-  DashBoard,
-  RegisterGmail,
+    Landing,
+    Register,
+    Menu,
+    Login,
+    Form,
+    About,
+    Detail,
+    DashBoard,
+    RegisterGmail,
 } from "./Views";
 import UserProfile from "./Views/UserProfile/userProfile";
 import Cart from "./Views/Cart/cart.jsx";
@@ -20,43 +20,45 @@ import NotFound from "./Components/NotFound/NotFound";
 //axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
+    const location = useLocation();
+    const roleUser = JSON.parse(window.localStorage.getItem("loggedInUser"));
 
-  const location = useLocation();
-  const roleUser= JSON.parse(window.localStorage.getItem('loggedInUser')); 
+    return (
+        <div className="App">
+            {location.pathname !== "/dashboard" && <NavBar />}
+            <Switch>
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/registergmail" component={RegisterGmail} />
+                <Route exact path="/menu" component={Menu} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/about" component={About} />
+                {(roleUser.role === "admin" ||
+                    roleUser.role === "superAdmin" ||
+                    roleUser.role === "user") && (
+                    <Route exact path="/profile" component={UserProfile} />
+                )}
 
-  return (
-    <div className="App">
-      {location.pathname !== "/login" &&
-        location.pathname !== "/register" &&
-        location.pathname !== "/dashboard" && <NavBar />}
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/registergmail" component={RegisterGmail} />
-        <Route exact path="/menu" component={Menu} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/about" component={About} />
-        { (roleUser.role === "admin" || roleUser.role === "superAdmin" ||
-        roleUser.role === "user" ) &&
-        <Route exact path="/profile" component={UserProfile} />}
+                <Route
+                    exact
+                    path="/product/:id"
+                    render={({ match }) => <Detail id={match.params.id} />}
+                />
 
-        <Route
-          exact
-          path="/product/:id"
-          render={({ match }) => <Detail id={match.params.id} />}
-        />
+                {(roleUser.role === "admin" ||
+                    roleUser.role === "superAdmin") && (
+                    <Route exact path="/dashboard" component={DashBoard} />
+                )}
+                <Route component={NotFound} />
 
-       { (roleUser.role === "admin" || roleUser.role === "superAdmin") && <Route exact path="/dashboard" component={DashBoard} />}
-        <Route component={NotFound} />
-          
-        <Route
-          exact
-          path="/cart/:userId"
-          render={({ match }) => <Cart id={match.params.userId} />}
-        />
-      </Switch>
-    </div>
-  );
+                <Route
+                    exact
+                    path="/cart/:userId"
+                    render={({ match }) => <Cart id={match.params.userId} />}
+                />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
