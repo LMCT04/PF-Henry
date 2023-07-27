8bf1e4a466d831d3deaba18307667c13e2bd5e1e
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2023-07-27 20:57:13 -0300

    xd

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/database.js");
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-server.listen('https://pf-backend-skye.onrender.com', async () => {
+server.listen(PORT, async () => {
   await conn.sync({ alter: true });
   console.log("%s listening at 3001");
 });
